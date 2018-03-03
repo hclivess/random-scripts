04fc0a3295280877d9fbd2ec810829d49509d3b8
Jan Kučera <translace@gmail.com>
2018-03-03 14:54:08 +0100

    Update ffmpeg-join.cmd

--- ffmpeg-join.cmd
+++ ffmpeg-join.cmd
@@ -1,2 +1,4 @@
-(for %%i in (*.mkv) do @echo file '%%i') > mylist.txt
-ffmpeg -f concat -safe 0 -i mylist.txt -c copy output.mkv
+(for %%i in (*.mp4) do @echo file '%%i') > mylist.txt
+for %%* in (.) do set CurrDirName=%%~nx*
+ffmpeg -f concat -safe 0 -i mylist.txt -c copy "%CurrDirName% - unabridged.mp4"
+pause
